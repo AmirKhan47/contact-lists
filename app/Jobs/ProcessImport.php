dcc232f65a2fc5abf63a6d583ecc9e11529f719b
Amir Khan <ballers999@gmail.com>
2022-10-08 14:51:40 +0500

    sms sending code

--- app/Jobs/ProcessImport.php
+++ app/Jobs/ProcessImport.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class ProcessImport implements ShouldQueue
 {
@@ -36,8 +37,12 @@ class ProcessImport implements ShouldQueue
      */
     public function handle()
     {
-        foreach ($this->data as $sale) {
-            $contactData = array_combine($this->header, $sale);
+        foreach ($this->data as $list) {
+            $contactData = array_combine($this->header, $list);
+            // send sms using twilio
+//            $message = $twilio->messages->create($contactData['phone'], ["body" => "Welcome to our portal.", "from" => "+12058561234"]);
+//            Log::debug($message->sid.' sms sent to '.$contactData['phone']);
+            Log::info('sms sent to '.$contactData['phone']);
             UserContact::create($contactData);
         }
     }
